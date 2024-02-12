ca437d9e379bdf884559dfb26527e7f4564edf56
Peter Guilhamet <peterguilhametjr@gmail.com>
2024-02-12 14:19:06 -0800

    made some global variables for easier access for the grader, and documented the code with some descriptive comments.

--- src/Database.java
+++ src/Database.java
@@ -6,8 +6,12 @@ import java.util.Random;
 public class Database{
     private Connection connection = null;
     private String dbName;
-
     private String dbms;
+    private String mysql_url = "jdbc:mysql://localhost:3306/";      //variable to change if you want to switch mysql url
+    private String mysql_user = "root";                             //mysql username, we used the default root but if you want to use your own mysql replace this variable
+    private String mysql_pass = "LTAndr3w";                         //mysql password, switch out for your own password
+    private String sqlite_url = "jdbc:sqlite:";                     //variable to change if you want to switch sqlite url
+
 
     public Database(String dbms, String db_name) {
         this.dbName = db_name;
@@ -15,24 +19,23 @@ public class Database{
         this.connectToDbms(dbms, db_name);
     }
 
+    //connects to the stated dbms
     public boolean connectToDbms(String dbms, String db_name) {
         if(dbms.equalsIgnoreCase("mysql")){
-            String url = "jdbc:mysql://localhost:3306/" + db_name;
-            String username = "root";
-            String password = "LTAndr3w";
+            String url = mysql_url + db_name;
             try{
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                this.connection = DriverManager.getConnection(url, username, password);
+                this.connection = DriverManager.getConnection(url, mysql_user, mysql_pass);
             } catch (Exception e){
                 System.out.println("encountered an error connecting to MySQL server at");
                 System.out.println("url: " + url);
-                System.out.println("user: " + username);
-                System.out.println("password: " + password);
+                System.out.println("user: " + mysql_user);
+                System.out.println("password: " + mysql_pass);
                 System.out.println(e);
             }
         }
         else{
-            String url = "jdbc:sqlite:" + db_name;
+            String url = sqlite_url + db_name;
             try{
                 Class.forName("org.sqlite.JDBC");
                 this.connection = DriverManager.getConnection(url);
@@ -46,6 +49,7 @@ public class Database{
         return connection == null;
     }
 
+    //insert rows, columns, and possibly and index for the first column in this method
     public void populateDB(String table_name, String type, int num_rows, int num_columns, boolean make_index) {
         if (this.connection == null) {
             System.out.print("db connection is null");
@@ -64,12 +68,8 @@ public class Database{
                 createTableSB.append("column" + String.valueOf(i) + " " + type + ", ");
             }
             createTableSB.append("column" + String.valueOf(num_columns - 1) + " " + type + ")");
-            // System.out.println("this is the create table statement: ");
-            // System.out.println(createTableSB.toString());
             statement.executeUpdate(createTableSB.toString());
             
-            // ToDo: populate the new table with rows
-            // format: INSERT INTO table_name (column1, column2, ...) VALUES (value1, value2, ...)
             // build the insert string using a string buffer
             StringBuffer insertSB = new StringBuffer();
             insertSB.append("INSERT INTO " + table_name + " (");
@@ -81,9 +81,8 @@ public class Database{
                 insertSB.append("?, ");
             }
             insertSB.append("?)");
-            // System.out.println("this is the insert statement");
-            // System.out.println(insertSB.toString());
 
+            //if make_index is true, create an index on the first column
             if (make_index) {
                 System.out.println("adding index on first column");
                 System.out.print("CREATE INDEX column0Idx ON " + table_name + " (column0)");
@@ -91,21 +90,19 @@ public class Database{
             }
 
             // build the batch using a prepared statement
-            this.connection.setAutoCommit(false);   // I think this makes it so that all of out data is committed
-                                                    // all at once, so we dont have a half executed query if there
-                                                    // is some sort of error
+            this.connection.setAutoCommit(false);
             PreparedStatement pStatement = this.connection.prepareStatement(insertSB.toString());
             
             
-            
+            //insert rows into the columns
             Random rand = new Random();
             for(int i = 0; i < num_rows; i++){
                 for(int j = 0; j< num_columns; j++){
-                    if (type.equals("INTEGER")){
+                    if (type.equals("INTEGER")){                                                                                                       //inserts random integers with the given restriction if type is int
                         pStatement.setInt(j + 1, rand.nextInt(2 * num_rows + 1));
-                    } else if (type.equals("FLOAT")){
+                    } else if (type.equals("FLOAT")){                                                                                                  //inserts random floats with the given restriction if type is float
                         pStatement.setFloat(j + 1, rand.nextFloat() * 2 - 1);
-                    } else {
+                    } else {                                                                                                                           //if type is varchar, insert 0 or 1 at random based on the size you provide for the varchar
                         pStatement.setString(j + 1, this.getRandomString(Integer.parseInt(type.substring(8, type.length() - 1))));
                     }
                 }
@@ -122,6 +119,7 @@ public class Database{
         }
     }
 
+    //returns random string for the varchar option
     public String getRandomString(int length) {
         StringBuffer temp = new StringBuffer();
         Random rand = new Random();
